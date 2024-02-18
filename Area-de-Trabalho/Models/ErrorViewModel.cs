c15bb0f78104914d0a74a289393d86a7e4ce7c9d
Xaglez <paulomontealtomac@gmail.com>
2024-02-18 18:04:44 -0300

    gpt-interface

---
+++ Area-de-Trabalho/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Area_de_Trabalho.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Area-de-Trabalho/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿function handleKeyPress(event) {
+    if (event.keyCode === 13) { // Verifica se a tecla pressionada é a tecla Enter
+        event.preventDefault(); // Impede o comportamento padrão do formulário
+
+        // Obtenha o valor do campo de entrada
+        var inputValue = document.getElementById("inputGPT").value;
+
+        document.getElementById("inputGPT").value = ""
+    }
+}
